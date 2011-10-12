2f1ec8a86d4c397c5f3a78cb2af92d831c5d2b93
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2011-10-12 19:24:54 +0000

    Added initial structure of XAMLite/Controls/XAMLiteGrid.cs
    git-svn-id: https://xamlite.svn.codeplex.com/svn@93542 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ XAMLite/Controls/XAMLiteGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XAMLite
+{
+    public class XAMLiteGrid : XAMLiteControl
+    {
+        public XAMLiteGrid(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
