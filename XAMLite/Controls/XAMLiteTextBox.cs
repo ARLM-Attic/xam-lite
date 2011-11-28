beb75e66075972101ba2845549f6e83fcc96226a
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2011-11-28 17:39:29 +0000

    Beginning to stub XAMLiteTextBox
    git-svn-id: https://xamlite.svn.codeplex.com/svn@94456 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ XAMLite/Controls/XAMLiteTextBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Windows.Media;
+using Color = Microsoft.Xna.Framework.Color;
+
+namespace XAMLite.Controls
+{
+    public class XAMLiteTextBox : XAMLiteControl
+    {
+        public XAMLiteTextBox(Game game)
+            : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="content"></param>
+        /// <param name="fontName"></param>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Draw(GameTime gameTime)
+        {
+            if (Visible == Visibility.Visible)
+            {
+
+            }
+        }
+    }
+}
