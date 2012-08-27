4fa4a42c9ff7898731328423d0b5b72126919bed
SND\adamkane_cp <SND\adamkane_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2012-08-27 20:17:47 +0000

    Added XAMLiteHelper.SafeDispose(control). This includes a null check before trying to dispose the XAMLiteControl instance.
    git-svn-id: https://xamlite.svn.codeplex.com/svn@99344 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ XAMLite/XAMLiteHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XAMLite
+{
+    /// <summary>
+    /// Static helper methods for XAMLite.
+    /// </summary>
+    public static class XAMLiteHelper
+    {
+        /// <summary>
+        /// Calls dispose, but does a null check first.
+        /// </summary>
+        /// <param name="control"></param>
+        public static void SafeDispose (XAMLiteControl control)
+        {
+            if (control != null)
+            {
+                control.Dispose();
+            }
+        }
+    }
+}
