056f3bfcc526bab510cc7506f5eeffcf7810d121
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2012-11-23 16:17:42 +0000

    XAMLiteCheckBox class now supports mouse hover for checked and unchecked checkboxes.
    CheckBoxSourceName
    CheckBoxHoverSourceName
    CheckBoxSelectedSourceName
    CheckBoxHoverSelectedSourceName
    
    git-svn-id: https://xamlite.svn.codeplex.com/svn@100501 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- XAMLite/Controls/XAMLiteCheckBox.cs
+++ XAMLite/Controls/XAMLiteCheckBox.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows.Media;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
 using Color = Microsoft.Xna.Framework.Color;
 
 namespace XAMLite
@@ -8,11 +9,18 @@ namespace XAMLite
     public class XAMLiteCheckBox : XAMLiteControl
     {
         /// <summary>
-        /// 
+        /// True when the checkbox is checked.
         /// </summary>
         public bool IsChecked { get; set; }
 
+        /// <summary>
+        /// Container for the checkbox texture asset.
+        /// </summary>
         private Rectangle _checkBox;
+
+        /// <summary>
+        /// Position for the text in relation to the CheckBox asset.
+        /// </summary>
         private Vector2 _textPos;
 
         /// <summary>
@@ -39,9 +47,14 @@ namespace XAMLite
             }
         }
 
-        // An idea for establishing a set of possible preloaded SpriteFonts??
+        /// <summary>
+        /// An idea for establishing a set of possible preloaded SpriteFonts??
+        /// </summary>
         private FontFamily _fontFamily;
 
+        /// <summary>
+        /// 
+        /// </summary>
         private bool _fontFamilyChanged; // used in the Update() method
 
         /// <summary>
@@ -61,29 +74,55 @@ namespace XAMLite
             }
         }
 
-        // character spacing
+        /// <summary>
+        /// character spacing
+        /// </summary>
         public int Spacing { get; set; }
 
         /// <summary>
-        /// This is the image file path, minus the file extension.
+        /// Texture asset for the checkbox when it is unchecked.
         /// </summary>
-        public string CheckBoxSourceName
-        {
-            get;
-            set;
-        }
+        private Texture2D _checkBoxUnchecked;
 
         /// <summary>
-        /// This is the image file path, minus the file extension.
+        /// This is the image file path, minus the file extension for an 
+        /// unchecked checkbox.
         /// </summary>
-        public string CheckBoxSelectedSourceName
-        {
-            get;
-            set;
-        }
+        public string CheckBoxSourceName { get; set; }
 
-        private Texture2D _checkBoxUnchecked;
+        /// <summary>
+        /// Texture asset for the checkbox when it is hovered over but 
+        /// unchecked.
+        /// </summary>
+        private Texture2D _checkBoxUncheckedHover;
+
+        /// <summary>
+        /// This is the image file path, minus the file extension for when the
+        /// checkbox is hovered over but is unchecked.
+        /// </summary>
+        public string CheckBoxHoverSourceName { get; set; }
+
+        /// <summary>
+        /// Texture asset for a checked checkbox.
+        /// </summary>
         private Texture2D _checkBoxChecked;
+
+        /// <summary>
+        /// This is the image file path, minus the file extension for when the 
+        /// checkbox is checked.
+        /// </summary>
+        public string CheckBoxSelectedSourceName { get; set; }
+
+        /// <summary>
+        /// Texture asset for a hovered and checked checkbox.
+        /// </summary>
+        private Texture2D _checkBoxCheckedHover;
+
+        /// <summary>
+        /// This is the image file path, minus the file extension for when the
+        /// checkbox is hovered over and checked.
+        /// </summary>
+        public string CheckBoxHoverSelectedSourceName { get; set; }
 
         /// <summary>
         /// 
@@ -140,8 +179,21 @@ namespace XAMLite
 
             RecalculateWidthAndHeight(Text);
 
-            _checkBoxChecked = Game.Content.Load<Texture2D>(CheckBoxSelectedSourceName);
+            Debug.Assert((CheckBoxSourceName != null), "Must set CheckBoxSourceName property. This is the image file path, minus the file extension.");
             _checkBoxUnchecked = Game.Content.Load<Texture2D>(CheckBoxSourceName);
+
+            Debug.Assert((CheckBoxSelectedSourceName != null), "Must set CheckBoxSelectedSourceName property. This is the image file path, minus the file extension.");
+            _checkBoxChecked = Game.Content.Load<Texture2D>(CheckBoxSelectedSourceName);
+
+            if (CheckBoxHoverSourceName != null)
+            {
+                _checkBoxUncheckedHover = Game.Content.Load<Texture2D>(CheckBoxHoverSourceName);
+            }
+
+            if (CheckBoxHoverSelectedSourceName != null)
+            {
+                _checkBoxCheckedHover = Game.Content.Load<Texture2D>(CheckBoxHoverSelectedSourceName);
+            }
 
             _checkBox = new Rectangle((int)Position.X, (int)Position.Y, _checkBoxChecked.Width, _checkBoxChecked.Height);
             _textPos = new Vector2((Position.X + _checkBox.Width + 10), Position.Y - 2.55f);
@@ -187,10 +239,20 @@ namespace XAMLite
                     SpriteFont.Spacing = Spacing;
                     SpriteBatch.DrawString(SpriteFont, Text, _textPos, _foregroundColor);
 
-                    SpriteBatch.Draw(
+                    if (_checkBoxCheckedHover != null && _checkBoxUncheckedHover != null)
+                    {
+                        SpriteBatch.Draw(MouseEntered ? IsChecked ? _checkBoxCheckedHover : _checkBoxUncheckedHover :
                         IsChecked ? _checkBoxChecked : _checkBoxUnchecked,
                         _checkBox,
                         (Color.White * (float)Opacity));
+                    }
+                    else
+                    {
+                        SpriteBatch.Draw(
+                        IsChecked ? _checkBoxChecked : _checkBoxUnchecked,
+                        _checkBox,
+                        (Color.White * (float)Opacity));
+                    }
                 }
                 else
                 {
