9a9f1eff364a43005cc9b7805e5b4cb8eb03c724
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2012-12-11 16:57:19 +0000

    Stubbed a XAMLiteComboBox and XAMLiteComboBoxItem class.
    git-svn-id: https://xamlite.svn.codeplex.com/svn@100808 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ XAMLite/Controls/Updated Controls/XAMLiteComboBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XAMLite
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class XAMLiteComboBox : XAMLiteBaseText
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsEditable { get; set; }
+
+        /// <summary>
+        /// List of Combo Box Items that make up the content of the control.
+        /// </summary>
+        public List<XAMLiteComboBoxItem> Items; 
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="game"></param>
+        public XAMLiteComboBox(Game game)
+            : base(game)
+        {
+            Items = new List<XAMLiteComboBoxItem>();
+        }
+    }
+}
---
+++ XAMLite/Controls/Updated Controls/XAMLiteComboBoxItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XAMLite
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class XAMLiteComboBoxItem : XAMLiteBaseContent
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="game"></param>
+        public XAMLiteComboBoxItem(Game game)
+            : base(game)
+        {
+        }
+    }
+}
