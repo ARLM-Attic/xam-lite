43b4476dc03e93b968dfebc4b0df3b42f79965f0
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2013-01-30 20:55:27 +0000

    Added the code side support for the newly added fonts.
    git-svn-id: https://xamlite.svn.codeplex.com/svn@101450 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- XAMLite/Fonts/CourierFonts.cs
+++ XAMLite/Fonts/CourierFonts.cs
@@ -14,6 +14,21 @@ namespace XAMLite
         protected static SpriteFont Courier10SpriteFont { get; private set; }
 
         /// <summary>
+        /// Courier 10 point Bold font
+        /// </summary>
+        protected static SpriteFont Courier10BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Courier 10 point Italic font
+        /// </summary>
+        protected static SpriteFont Courier10ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Courier 10 point Bold Italic font
+        /// </summary>
+        protected static SpriteFont Courier10BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Courier 12 point font
         /// </summary>
         protected static SpriteFont Courier12SpriteFont { get; private set; }
@@ -39,11 +54,29 @@ namespace XAMLite
         protected static SpriteFont Courier20SpriteFont { get; private set; }
 
         /// <summary>
+        /// Courier 20 point Bold font.
+        /// </summary>
+        protected static SpriteFont Courier20BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Courier 20 point Italic font.
+        /// </summary>
+        protected static SpriteFont Courier20ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Courier 20 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Courier20BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Loads the Courier fonts.
         /// </summary>
         private void LoadCourierFonts()
         {
             Courier10SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Courier10");
+            Courier10BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Courier10Bold");
+            Courier10ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Courier10Italic");
+            Courier10BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Courier10BoldItalic");
             Courier20SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Courier20");
         }
 
@@ -57,8 +90,38 @@ namespace XAMLite
                 case "Courier10":
                     SpriteFont = Courier10SpriteFont;
                     break;
+                case "Courier10Bold":
+                    SpriteFont = Courier10BoldSpriteFont;
+                    break;
+                case "Courier10Italic":
+                    SpriteFont = Courier10ItalicSpriteFont;
+                    break;
+                case "Courier10BoldItalic":
+                    SpriteFont = Courier10BoldItalicSpriteFont;
+                    break;
+                case "Courier12":
+                    SpriteFont = Courier12SpriteFont;
+                    break;
+                case "Courier12Bold":
+                    SpriteFont = Courier12BoldSpriteFont;
+                    break;
+                case "Courier12Italic":
+                    SpriteFont = Courier12ItalicSpriteFont;
+                    break;
+                case "Courier12BoldItalic":
+                    SpriteFont = Courier12BoldItalicSpriteFont;
+                    break;
                 case "Courier20":
                     SpriteFont = Courier20SpriteFont;
+                    break;
+                case "Courier20Bold":
+                    SpriteFont = Courier20BoldSpriteFont;
+                    break;
+                case "Courier20Italic":
+                    SpriteFont = Courier20ItalicSpriteFont;
+                    break;
+                case "Courier20BoldItalic":
+                    SpriteFont = Courier20BoldItalicSpriteFont;
                     break;
                 default:
                     throw new Exception("Courier font not supported.");
--- XAMLite/Fonts/VerdanaFonts.cs
+++ XAMLite/Fonts/VerdanaFonts.cs
@@ -19,6 +19,16 @@ namespace XAMLite
         protected static SpriteFont Verdana10BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 10 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana10ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 10 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana10BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Verdana 11 point font.
         /// </summary>
         protected static SpriteFont Verdana11SpriteFont { get; private set; }
@@ -27,6 +37,16 @@ namespace XAMLite
         /// Verdana 11 point Bold font.
         /// </summary>
         protected static SpriteFont Verdana11BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 11 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana11ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 11 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana11BoldItalicSpriteFont { get; private set; }
 
         /// <summary>
         /// Verdana 12 point font.
@@ -39,6 +59,16 @@ namespace XAMLite
         protected static SpriteFont Verdana12BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 12 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana12ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 12 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana12BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Verdana 13 point font.
         /// </summary>
         protected static SpriteFont Verdana13SpriteFont { get; private set; }
@@ -47,6 +77,16 @@ namespace XAMLite
         /// Verdana 13 point Bold font.
         /// </summary>
         protected static SpriteFont Verdana13BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 13 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana13ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 13 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana13BoldItalicSpriteFont { get; private set; }
 
         /// <summary>
         /// Verdana 14 point font.
@@ -59,9 +99,34 @@ namespace XAMLite
         protected static SpriteFont Verdana14BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 14 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana14ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 14 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana14BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Verdana 15 point font.
         /// </summary>
         protected static SpriteFont Verdana15SpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 15 point Bold font.
+        /// </summary>
+        protected static SpriteFont Verdana15BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 15 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana15ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 15 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana15BoldItalicSpriteFont { get; private set; }
 
         /// <summary>
         /// Verdana 16 point font.
@@ -74,6 +139,36 @@ namespace XAMLite
         protected static SpriteFont Verdana16BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 16 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana16ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 16 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana16BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 18 point font.
+        /// </summary>
+        protected static SpriteFont Verdana18SpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 18 point Bold font.
+        /// </summary>
+        protected static SpriteFont Verdana18BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 18 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana18ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 18 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana18BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Verdana 20 point font.
         /// </summary>
         protected static SpriteFont Verdana20SpriteFont { get; private set; }
@@ -84,9 +179,34 @@ namespace XAMLite
         protected static SpriteFont Verdana20BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 20 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana20ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 20 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana20BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 24 point font.
+        /// </summary>
+        protected static SpriteFont Verdana24SpriteFont { get; private set; }
+
+        /// <summary>
         /// Verdana 24 point Bold font.
         /// </summary>
         protected static SpriteFont Verdana24BoldSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 24 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana24ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 24 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana24BoldItalicSpriteFont { get; private set; }
 
         /// <summary>
         /// Verdana 60 point font.
@@ -99,28 +219,74 @@ namespace XAMLite
         protected static SpriteFont Verdana60BoldSpriteFont { get; private set; }
 
         /// <summary>
+        /// Verdana 60 point Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana60ItalicSpriteFont { get; private set; }
+
+        /// <summary>
+        /// Verdana 60 point Bold Italic font.
+        /// </summary>
+        protected static SpriteFont Verdana60BoldItalicSpriteFont { get; private set; }
+
+        /// <summary>
         /// Loads the Verdana fonts.
         /// </summary>
         private void LoadVerdanaFonts()
         { 
             Verdana10SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana10");
             Verdana10BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana10Bold");
+            Verdana10ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana10Italic"); 
+            Verdana10BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana10BoldItalic");
+
             Verdana11SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana11");
             Verdana11BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana11Bold");
+            Verdana11ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana11Italic");
+            Verdana11BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana11BoldItalic");
+
             Verdana12SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana12");
             Verdana12BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana12Bold");
+            Verdana12ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana12Italic");
+            Verdana12BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana12BoldItalic");
+
             Verdana13SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana13");
             Verdana13BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana13Bold");
+            Verdana13ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana13Italic");
+            Verdana13BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana13BoldItalic");
+
             Verdana14SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana14");
             Verdana14BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana14Bold");
+            Verdana14ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana14Italic");
+            Verdana14BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana14BoldItalic");
+
             Verdana15SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana15");
+            Verdana15BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana15Bold");
+            Verdana15ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana15Italic");
+            Verdana15BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana15BoldItalic");
+
             Verdana16SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana16");
             Verdana16BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana16Bold");
+            Verdana16ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana16Italic");
+            Verdana16BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana16BoldItalic");
+
+            Verdana18SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana18");
+            Verdana18BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana18Bold");
+            Verdana18ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana18Italic");
+            Verdana18BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana18BoldItalic");
+
             Verdana20SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana20");
             Verdana20BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana20Bold");
+            Verdana20ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana20Italic");
+            Verdana20BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana20BoldItalic");
+
+            Verdana24SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana24");
             Verdana24BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana24Bold");
+            Verdana24ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana24Italic");
+            Verdana24BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana24BoldItalic");
+
             Verdana60SpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana60");
             Verdana60BoldSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana60Bold");
+            Verdana60ItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana60Italic");
+            Verdana60BoldItalicSpriteFont = Game.Content.Load<SpriteFont>("Fonts/Verdana60BoldItalic");
         }
 
         /// <summary>
@@ -136,11 +302,23 @@ namespace XAMLite
                 case "Verdana10Bold":
                     SpriteFont = Verdana10BoldSpriteFont;
                     break;
+                case "Verdana10Italic":
+                    SpriteFont = Verdana10ItalicSpriteFont;
+                    break;
+                case "Verdana10BoldItalic":
+                    SpriteFont = Verdana10BoldItalicSpriteFont;
+                    break;
                 case "Verdana11":
                     SpriteFont = Verdana11SpriteFont;
                     break;
                 case "Verdana11Bold":
                     SpriteFont = Verdana11BoldSpriteFont;
+                    break;
+                case "Verdana11Italic":
+                    SpriteFont = Verdana11ItalicSpriteFont;
+                    break;
+                case "Verdana11BoldItalic":
+                    SpriteFont = Verdana11BoldItalicSpriteFont;
                     break;
                 case "Verdana12":
                     SpriteFont = Verdana12SpriteFont;
@@ -148,11 +326,23 @@ namespace XAMLite
                 case "Verdana12Bold":
                     SpriteFont = Verdana12BoldSpriteFont;
                     break;
+                case "Verdana12Italic":
+                    SpriteFont = Verdana12ItalicSpriteFont;
+                    break;
+                case "Verdana12BoldItalic":
+                    SpriteFont = Verdana12BoldItalicSpriteFont;
+                    break;
                 case "Verdana13":
                     SpriteFont = Verdana13SpriteFont;
                     break;
                 case "Verdana13Bold":
                     SpriteFont = Verdana13BoldSpriteFont;
+                    break;
+                case "Verdana13Italic":
+                    SpriteFont = Verdana13ItalicSpriteFont;
+                    break;
+                case "Verdana13BoldItalic":
+                    SpriteFont = Verdana13BoldItalicSpriteFont;
                     break;
                 case "Verdana14":
                     SpriteFont = Verdana14SpriteFont;
@@ -160,8 +350,23 @@ namespace XAMLite
                 case "Verdana14Bold":
                     SpriteFont = Verdana14BoldSpriteFont;
                     break;
+                case "Verdana14Italic":
+                    SpriteFont = Verdana14ItalicSpriteFont;
+                    break;
+                case "Verdana14BoldItalic":
+                    SpriteFont = Verdana14BoldItalicSpriteFont;
+                    break;
                 case "Verdana15":
                     SpriteFont = Verdana15SpriteFont;
+                    break;
+                case "Verdana15Bold":
+                    SpriteFont = Verdana15BoldSpriteFont;
+                    break;
+                case "Verdana15Italic":
+                    SpriteFont = Verdana15ItalicSpriteFont;
+                    break;
+                case "Verdana15BoldItalic":
+                    SpriteFont = Verdana15BoldItalicSpriteFont;
                     break;
                 case "Verdana16":
                     SpriteFont = Verdana16SpriteFont;
@@ -169,11 +374,35 @@ namespace XAMLite
                 case "Verdana16Bold":
                     SpriteFont = Verdana16BoldSpriteFont;
                     break;
+                case "Verdana16Italic":
+                    SpriteFont = Verdana16ItalicSpriteFont;
+                    break;
+                case "Verdana16BoldItalic":
+                    SpriteFont = Verdana16BoldItalicSpriteFont;
+                    break;
+                case "Verdana18":
+                    SpriteFont = Verdana16SpriteFont;
+                    break;
+                case "Verdana18Bold":
+                    SpriteFont = Verdana16BoldSpriteFont;
+                    break;
+                case "Verdana18Italic":
+                    SpriteFont = Verdana16ItalicSpriteFont;
+                    break;
+                case "Verdana18BoldItalic":
+                    SpriteFont = Verdana16BoldItalicSpriteFont;
+                    break;
                 case "Verdana20":
                     SpriteFont = Verdana20SpriteFont;
                     break;
                 case "Verdana20Bold":
                     SpriteFont = Verdana20BoldSpriteFont;
+                    break;
+                case "Verdana20Italic":
+                    SpriteFont = Verdana20ItalicSpriteFont;
+                    break;
+                case "Verdana20BoldItalic":
+                    SpriteFont = Verdana20BoldItalicSpriteFont;
                     break;
                 case "Verdana24Bold":
                     SpriteFont = Verdana24BoldSpriteFont;
@@ -183,6 +412,12 @@ namespace XAMLite
                     break;
                 case "Verdana60Bold":
                     SpriteFont = Verdana60BoldSpriteFont;
+                    break;
+                case "Verdana60Italic":
+                    SpriteFont = Verdana60ItalicSpriteFont;
+                    break;
+                case "Verdana60BoldItalic":
+                    SpriteFont = Verdana60BoldItalicSpriteFont;
                     break;
                 default:
                     throw new Exception("Verdana font not supported.");
