db3b8fa990e459ab0433c86bde7b0f1c74412741
SND\ScottLaForge_cp <SND\ScottLaForge_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2013-02-13 14:49:31 +0000

    Stubbed the slider class
    git-svn-id: https://xamlite.svn.codeplex.com/svn@101688 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ XAMLite/Controls/Updated Controls/XAMLiteSlider.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="XAMLiteSlider.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace XAMLite.Controls.Updated_Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class XAMLiteSlider : XAMLiteBaseControl
+    {
+        public XAMLiteSlider(Game game)
+            : base(game)
+        {
+        }
+    }
+}
